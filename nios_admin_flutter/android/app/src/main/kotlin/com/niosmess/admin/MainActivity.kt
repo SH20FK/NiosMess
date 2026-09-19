e7fad74b0821130f840d29fbbe2d02eced005191
sh20fk <sh20fk@users.noreply.github.com>
2026-09-19 12:14:06 +0500

    chore: remove obsolete nios_admin_flutter, niosmess_landing, record_linux, and stagehand

--- nios_admin_flutter/android/app/src/main/kotlin/com/niosmess/admin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.niosmess.admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- nios_admin_flutter/lib/core/localization/l10n.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:nios_admin_flutter/l10n/app_localizations.dart';
-
-extension L10nContext on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
-}
